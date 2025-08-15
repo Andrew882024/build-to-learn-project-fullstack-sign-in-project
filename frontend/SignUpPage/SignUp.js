e6c226bfd83de230e41369e48205cb97a807ab0f
Andrew882024 <acz882024@gmail.com>
2025-08-14 17:59:42 -0700

    add some javascript to Sign Up Page

---
+++ frontend/SignUpPage/SignUp.js
@@ -0,0 +1,8 @@
+let InputUsername = null;
+let InputPassword = null;
+
+let SignUpButton = document.getElementById("sign_in_button_button");
+SignUpButton.addEventListener("click",()=>{
+  InputUsername = document.getElementById("user_name_id");
+  InputPassword = document.getElementById("password_id");
+})
