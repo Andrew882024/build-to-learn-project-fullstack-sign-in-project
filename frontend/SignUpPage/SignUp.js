fa32c5915e96968e47b8fa52abdaddff0fa7ac53
Andrew882024 <acz882024@gmail.com>
2025-08-15 17:50:00 -0700

    better sign up

--- frontend/SignUpPage/SignUp.js
+++ frontend/SignUpPage/SignUp.js
@@ -2,28 +2,51 @@ let InputUsername = null;
 let InputPassword = null;
 
 let SignUpButton = document.getElementById("sign_in_button_button");
-SignUpButton.addEventListener("click",()=>{
+SignUpButton.addEventListener("click",async ()=>{
   InputUsername = document.getElementById("user_name_id").value;
   InputPassword = document.getElementById("password_id").value;
 
-  console.log(InputUsername);
+  if(InputUsername.length<5 || InputUsername.length>20){
+    alert("invalid username, your username length should be bigger than 5 "+
+      "and smaller than 20");
+    return;
+  }
+  if(InputUsername.includes(" ")){
+    alert("invalid username, your username should not include space in it");
+  }
+  if((/[0-9]/.test(InputPassword)) != true){
+    alert("invalid password, your password should have at least one number");
+    return;
+  }
+  if(InputPassword.includes(" ")){
+    alert("invalid password, your password should not include space in it");
+    return;
+  }
 
-  fetch(`http://127.0.0.1:8000/username_check_if_exist/${InputUsername}`)
+  console.log(InputUsername);
+  let functionEnd = false;
+  await fetch(`http://127.0.0.1:8000/username_check_if_exist/${InputUsername}`)
   .then((temp)=>temp.json())
   .then((whether_exist)=>{
     console.log(whether_exist);
     if(whether_exist === "exist"){
       alert("the username already exist, try something else");
+      functionEnd = true;
     }
     else if(whether_exist === "not_exist"){
-      alert("not exist");
+      console.log("this username does not exist before, the username is ok");
     }
     else{
       alert("there is an error");
+      functionEnd = true;
     }
   })
+
+  if(functionEnd === true){
+    return;
+  }
   
-  fetch("http://127.0.0.1:8000/get_username_and_password", {
+  await fetch("http://127.0.0.1:8000/get_username_and_password", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -33,5 +56,8 @@ SignUpButton.addEventListener("click",()=>{
       password:InputPassword
     })
   });
+
+  alert("you are signed up");
+  return;
   
 })
