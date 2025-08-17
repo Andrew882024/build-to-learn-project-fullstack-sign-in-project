b98d3bdb69423dbe29d43133b08f1806e0f54a7c
Andrew882024 <acz882024@gmail.com>
2025-08-16 22:29:44 -0700

    connect python to mysql

--- backend/SignUp.py
+++ backend/SignUp.py
@@ -3,6 +3,57 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
+#for connecting mysql
+import mysql.connector
+
+
+#//////////////////////////////////////////////////////////////////////////////#
+#                              database function                               #
+#//////////////////////////////////////////////////////////////////////////////#
+
+mydb = mysql.connector.connect(host="localhost",user="root",passwd="root",database="build_to_learn_sign_in_database")
+
+mycursor = mydb.cursor()
+
+def add_new_user_infor(primary_key_column_name:str,other_one_column_name:str,
+  primary_key_value:str,other_one_column_value):
+  mycursor.execute(f"insert into user_infor({primary_key_column_name},{other_one_column_name}) values('{primary_key_value}','{other_one_column_value}')")
+  mydb.commit()
+  return
+
+#this thing is working
+#add_new_user_infor("username","passwd","a_username","a_password")
+
+def whether_this_item_exist_in_this_column(column_name:str,item:str) ->bool:
+  mycursor.execute(f"SELECT EXISTS(SELECT * FROM user_infor WHERE {column_name}='{item}');")
+  output = mycursor.fetchone() 
+  #print(output[0])
+  if(output[0] == 1):
+    return True
+  else:
+    return False
+
+
+
+#whether_this_item_exist_in_this_column("username","a_usernam")
+
+def whether_password_match_username(username:str,password:str):
+  mycursor.execute(f"SELECT passwd FROM user_infor WHERE username='{username}'")
+  output = mycursor.fetchone()
+  #print(output[0])
+  if(output[0] == password):
+    print(1)
+    return True
+  else:
+    print(0)
+    return False
+  
+
+#whether_password_match_username("a_username","a_passwor")  
+
+#//////////////////////////////////////////////////////////////////////////////#
+#                                  api part                                    #
+#//////////////////////////////////////////////////////////////////////////////#
 
 app = FastAPI()
 
@@ -49,4 +100,11 @@ def check_whether_username_and_password_match(username:str,password:str):
 def get_username_and_password(item:UserNameAndPassword):
   username_and_passworrd_dictionary[item.username] = item.password
   return f"{item.username} and {item.password}"
+
+
+
+
+
+
+
 
